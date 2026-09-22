0a5b2c22dfd95bf39c3b5d4cf944b809df4723e4
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2026-09-23 00:35:38 +0200

    manage cmd line arguments through iterateAllocator

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,9 @@
 const std = @import("std");
+const Writer = std.Io.Writer;
 
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
-    //const gpa = init.gpa;
+    const gpa = init.gpa;
 
     var stdout_buffer_writer: [1024]u8 = undefined;
     var stdout_writer: std.Io.File.Writer = .initStreaming(.stdout(), io, &stdout_buffer_writer);
@@ -17,9 +18,33 @@ pub fn main(init: std.process.Init) !void {
     var stderr_writer: std.Io.File.Writer = .initStreaming(.stderr(), io, &stderr_buffer_writer);
     const stderr = &stderr_writer.interface;
 
-    try stdout.writeAll("stdout test.\n");
-    try stdout.flush();
+    var args = try init.minimal.args.iterateAllocator(gpa);
+    defer args.deinit();
 
-    try stderr.writeAll("stderr test.\n");
-    try stderr.flush();
+    const prog_name = args.next() orelse "zee";
+    _ = prog_name;
+
+    const next_arg = args.next() orelse {
+        try showUsage(stderr);
+        return;
+    };
+
+    if (std.mem.eql(u8, next_arg, "-h") or std.mem.eql(u8, next_arg, "--help")) {
+        try showUsage(stdout);
+        return;
+    }
+}
+
+const help_text =
+    \\Usage: tee [OPTION]... [FILE]...
+    \\Copy standard input to each FILE, and also to starnard output.
+    \\
+    \\-a, --append      append to the given FILEs, do not overwirte.
+    \\-h, --help        display this help and exit
+    \\
+;
+pub fn showUsage(writer: *Writer) !void {
+    try writer.print("{s}", .{help_text});
+    try writer.flush();
+    return;
 }
